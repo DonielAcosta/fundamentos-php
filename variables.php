e3a866739612bcacf506ca41163acc25223d5eb5
Doniel Acosta <donielacosta1995@gmail.com>
2025-11-20 21:08:27 -0400

    variables

--- variables.php
+++ variables.php
@@ -1,11 +1,15 @@
+<?php
+    $curso = "Curso  Profesional de PHP y Laravel";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso PHP</title>
+    <title><?= $curso; ?></title>
 </head>
 <body>
-    <h1> Bienvenido al Curso PHP</h1>
+    <h1> Bienvenido al <?= $curso ?></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati consequuntur veritatis, unde omnis vel numquam, amet sed molestias sit repellat perspiciatis ullam assumenda voluptatem dolore. Obcaecati quisquam corporis repudiandae eos!</p>
 </body>
 </html>
